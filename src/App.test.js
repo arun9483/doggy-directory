b6eaf745686ea5e15e2ccb8a1b1c3673b1b98b8a
Arun Kumar Tripathi <arun9483@gmail.com>
2022-11-05 16:49:20 +0530

    [ADD] test landing page - h1, select, button and img

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,13 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders the landing page", () => {
-  render(<App />);
+describe('landing page', () => {
+  test('verify landing page elements', () => {
+    render(<App />);
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/Doggy Directory/);
+    expect(screen.getByRole('combobox')).toHaveDisplayValue(/Select a breed/);
+    expect(screen.getByRole('button', { name: 'Search' })).toBeDisabled();
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
 });
