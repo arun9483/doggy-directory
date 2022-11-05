0387467b0f47d6e24d2bda4c5ac3d3e92c77f247
Arun Kumar Tripathi <arun9483@gmail.com>
2022-11-05 17:23:49 +0530

    [ADD]: test async functionality - mock fetch, handle act warning, test husky option post api call and state update

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import mockFetch from './mocks/mockFetch';
 
 describe('landing page', () => {
-  test('verify landing page elements', () => {
+  beforeEach(() => {
+    jest.spyOn(window, 'fetch').mockImplementation(mockFetch);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('verify landing page elements', async () => {
     render(<App />);
 
     expect(screen.getByRole('heading')).toHaveTextContent(/Doggy Directory/);
     expect(screen.getByRole('combobox')).toHaveDisplayValue(/Select a breed/);
+
+    const optionHusky = await screen.findByRole('option', {
+      name: 'husky', // note option husky is available in our mock response
+    });
+
+    expect(optionHusky).toBeInTheDocument();
+
     expect(screen.getByRole('button', { name: 'Search' })).toBeDisabled();
     expect(screen.getByRole('img')).toBeInTheDocument();
   });
